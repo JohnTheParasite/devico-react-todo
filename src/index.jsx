49705476cb10c7cc2048c1d3434469ac7e59e960
i.barbashov <ivan.barbashov@gmail.com>
2022-06-01 17:14:00 +0300

    init

---
+++ src/index.jsx
@@ -0,0 +1,26 @@
+import React from "react"
+import { createRoot } from "react-dom/client"
+
+import "@/styles/styles"
+
+const App = () => (
+  <div className="container">
+    <h1>Webpack course</h1>
+    <hr />
+    <div className="logo"></div>
+    <div className="card">
+      <h2>
+        SCSS
+      </h2>
+    </div>
+  </div>
+)
+
+const container = document.getElementById('app');
+const root = createRoot(container);
+root.render(<App />);
+
+const devMode = process.env.NODE_ENV === 'development';
+if (devMode && module && module.hot) {
+  module.hot.accept();
+}
