9e56f71888c02c80b048cdbb8aa1e5d8c4475abf
i.barbashov <ivan.barbashov@gmail.com>
2022-07-07 16:20:02 +0300

    react-hook-form -> Formik

---
+++ src/components/LoginForm/index-react-hook-form.tsx
@@ -0,0 +1,207 @@
+import React, { useState } from 'react'
+import styles from './styles.module.scss'
+import logo from '@/images/logo.svg'
+import { Button, TextField, Alert } from '@mui/material'
+import { Link, useNavigate } from 'react-router-dom'
+import { useForm } from 'react-hook-form'
+import { yupResolver } from '@hookform/resolvers/yup'
+import { useDispatch } from 'react-redux'
+import * as yup from 'yup'
+import Api from '@/services/api'
+import { AxiosResponse, AxiosError } from 'axios'
+import { ErrorMessage, UserResponse } from '@/redux/Types'
+import { setCurrentUser } from '@/redux/actions'
+
+const schemaReg = yup.object().shape({
+  login: yup.string(),
+  email: yup.string().email().required(),
+  password: yup.string().min(4).max(15).required(),
+  confirmPassword: yup.string().oneOf([yup.ref('password'), null]),
+})
+
+const schemaLog = yup.object().shape({
+  email: yup.string().email().required(),
+  password: yup.string().min(4).max(15).required(),
+})
+
+interface IFormInputReg {
+  login: string
+  email: string
+  password: string
+  confirmPassword: string
+}
+
+interface IFormInputLog {
+  email: string
+  password: string
+}
+
+export default function LoginForm({ formType }: { formType: string }) {
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
+
+  let validationError = ''
+  const [loginError, setLoginError] = useState('')
+
+  type IFormInput = IFormInputReg | IFormInputLog
+
+  const schema = formType === 'registration' ? schemaReg : schemaLog
+
+  const formMethods = useForm<IFormInput>({
+    resolver: yupResolver(schema),
+  })
+
+  const onSubmit = (data: IFormInput) => {
+    if (formType === 'registration') {
+      const regData = data as IFormInputReg
+      regData.login = regData.login === '' ? regData.email.split('@')[0] : regData.login
+      registration(regData)
+    } else {
+      login(data as IFormInputLog)
+    }
+  }
+
+  const login = (data: IFormInputLog) => {
+    const { email, password } = data
+    Api.authorize(email, password)
+      .then((res: AxiosResponse<UserResponse>) => {
+        if (res?.status === 200) {
+          dispatch(setCurrentUser(res.data.user))
+          localStorage.setItem('token', res.data.accessToken)
+          localStorage.setItem('refreshToken', res.data.refreshToken)
+          navigate('/todos')
+        }
+      })
+      .catch((res: AxiosError<ErrorMessage>) => {
+        let error = ''
+        if (res.response?.status === 400) {
+          error = res.response?.data.message
+        }
+        setLoginError(error)
+      })
+  }
+
+  const registration = (data: IFormInputReg) => {
+    const { login, email, password } = data
+    Api.register(login, email, password)
+      .then((res: AxiosResponse<UserResponse>) => {
+        if (res.status === 200) {
+          dispatch(setCurrentUser(res.data.user))
+          localStorage.setItem('token', res.data.accessToken)
+          localStorage.setItem('refreshToken', res.data.refreshToken)
+          navigate('/todos')
+        }
+      })
+      .catch((res: AxiosError<ErrorMessage>) => {
+        let error = ''
+        if (res.response?.status === 400) {
+          error = res.response?.data.message
+        }
+        setLoginError(error)
+      })
+  }
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = formMethods
+
+  if (formType === 'login') {
+    if ('email' in errors) {
+      validationError = 'Email must be valid'
+    } else if ('password' in errors) {
+      validationError = errors.password?.message as string
+    }
+  }
+
+  const alertMessage = validationError.length ? <Alert severity="error">{validationError}</Alert> : ''
+  const loginMessage = loginError.length ? <Alert severity="error">{loginError}</Alert> : ''
+
+  const loginTextField = <TextField label="Login" variant="standard" type="text" size="small" {...register('login')} />
+
+  const confirmPasswordTextField = (
+    <TextField
+      label="Confirm password"
+      variant="standard"
+      required={true}
+      type="password"
+      size="small"
+      error={'confirmPassword' in errors}
+      helperText={'confirmPassword' in errors ? 'The confirmation password does not match' : ''}
+      {...register('confirmPassword')}
+    />
+  )
+
+  const emailTextFieldReg = (
+    <TextField
+      label="Email"
+      variant="standard"
+      required={true}
+      placeholder="John.Doe@gmail.com"
+      type="text"
+      size="small"
+      error={'email' in errors}
+      helperText={'email' in errors ? 'Email must be valid' : ''}
+      {...register('email')}
+    />
+  )
+
+  const emailTextFieldLog = (
+    <TextField
+      label="Email"
+      variant="standard"
+      required={true}
+      placeholder="John.Doe@gmail.com"
+      type="text"
+      size="small"
+      {...register('email')}
+    />
+  )
+
+  const LinkContainer =
+    formType === 'registration' ? (
+      <div className={styles['link-container']}>
+        <div>Already a user?</div>
+        <Link to="/" className={styles.link}>
+          LOGIN
+        </Link>
+      </div>
+    ) : (
+      <div className={styles['link-container']}>
+        <div>Need an account?</div>
+        <Link to="/registration" className={styles.link}>
+          SIGN UP
+        </Link>
+      </div>
+    )
+
+  return (
+    <div className={styles.inner}>
+      <div className={styles.login}>
+        <div className={styles.logo}>
+          <img src={logo} className={styles['App-logo']} alt="logo" />
+        </div>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          {formType === 'registration' ? loginTextField : ''}
+          {formType === 'registration' ? emailTextFieldReg : emailTextFieldLog}
+          <TextField
+            label="Password"
+            variant="standard"
+            required={true}
+            type="password"
+            size="small"
+            {...register('password')}
+          />
+          {formType === 'registration' ? confirmPasswordTextField : ''}
+          {alertMessage}
+          {loginMessage}
+          <Button variant="contained" className={styles.button} type="submit">
+            {formType === 'registration' ? 'Sign up' : 'Login'}
+          </Button>
+          {LinkContainer}
+        </form>
+      </div>
+    </div>
+  )
+}
--- src/components/LoginForm/index.tsx
+++ src/components/LoginForm/index.tsx
@@ -1,67 +1,64 @@
 import React, { useState } from 'react'
-import styles from './styles.module.scss'
-import logo from '@/images/logo.svg'
 import { Button, TextField, Alert } from '@mui/material'
-import { Link, useNavigate } from 'react-router-dom'
-import { useForm } from 'react-hook-form'
-import { yupResolver } from '@hookform/resolvers/yup'
-import { useDispatch } from 'react-redux'
+import { useFormik, Field, FormikProvider } from 'formik'
 import * as yup from 'yup'
+import logo from '@/images/logo.svg'
+import { Link, useNavigate } from 'react-router-dom'
 import Api from '@/services/api'
-import { AxiosResponse, AxiosError } from 'axios'
+import { AxiosError, AxiosResponse } from 'axios'
 import { ErrorMessage, UserResponse } from '@/redux/Types'
 import { setCurrentUser } from '@/redux/actions'
+import { useDispatch } from 'react-redux'
+import styles from '@/components/LoginForm/styles.module.scss'
 
-const schemaReg = yup.object().shape({
+const schemaRegistration = yup.object().shape({
   login: yup.string(),
-  email: yup.string().email().required(),
-  password: yup.string().min(4).max(15).required(),
+  email: yup.string().email('Enter a valid email').required('Email is required'),
+  password: yup.string().min(4, 'Password should be of minimum 4 characters length').required('Password is required'),
   confirmPassword: yup.string().oneOf([yup.ref('password'), null]),
 })
 
-const schemaLog = yup.object().shape({
-  email: yup.string().email().required(),
-  password: yup.string().min(4).max(15).required(),
+const schemaLogin = yup.object().shape({
+  email: yup.string().email('Enter a valid email').required('Email is required'),
+  password: yup.string().min(4, 'Password should be of minimum 4 characters length').required('Password is required'),
 })
 
-interface IFormInputReg {
+interface IFormInput {
   login: string
   email: string
   password: string
   confirmPassword: string
 }
 
-interface IFormInputLog {
-  email: string
-  password: string
-}
-
 export default function LoginForm({ formType }: { formType: string }) {
   const dispatch = useDispatch()
   const navigate = useNavigate()
 
-  let validationError = ''
   const [loginError, setLoginError] = useState('')
 
-  type IFormInput = IFormInputReg | IFormInputLog
+  const schema = formType === 'registration' ? schemaRegistration : schemaLogin
 
-  const schema = formType === 'registration' ? schemaReg : schemaLog
+  const initialValues = (
+    formType === 'registration'
+      ? { login: '', email: '', password: '', confirmPassword: '' }
+      : { email: '', password: '' }
+  ) as IFormInput
 
-  const formMethods = useForm<IFormInput>({
-    resolver: yupResolver(schema),
+  const formik = useFormik({
+    initialValues: initialValues as IFormInput,
+    validationSchema: schema,
+    onSubmit: (values: IFormInput) => {
+      if (formType === 'registration') {
+        const regData = values
+        regData.login = regData.login === '' ? regData.email.split('@')[0] : regData.login
+        registration(regData)
+      } else {
+        login(values)
+      }
+    },
   })
 
-  const onSubmit = (data: IFormInput) => {
-    if (formType === 'registration') {
-      const regData = data as IFormInputReg
-      regData.login = regData.login === '' ? regData.email.split('@')[0] : regData.login
-      registration(regData)
-    } else {
-      login(data as IFormInputLog)
-    }
-  }
-
-  const login = (data: IFormInputLog) => {
+  const login = (data: IFormInput) => {
     const { email, password } = data
     Api.authorize(email, password)
       .then((res: AxiosResponse<UserResponse>) => {
@@ -81,7 +78,7 @@ export default function LoginForm({ formType }: { formType: string }) {
       })
   }
 
-  const registration = (data: IFormInputReg) => {
+  const registration = (data: IFormInput) => {
     const { login, email, password } = data
     Api.register(login, email, password)
       .then((res: AxiosResponse<UserResponse>) => {
@@ -101,61 +98,51 @@ export default function LoginForm({ formType }: { formType: string }) {
       })
   }
 
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = formMethods
-
-  if (formType === 'login') {
-    if ('email' in errors) {
-      validationError = 'Email must be valid'
-    } else if ('password' in errors) {
-      validationError = errors.password?.message as string
-    }
-  }
-
-  const alertMessage = validationError.length ? <Alert severity="error">{validationError}</Alert> : ''
   const loginMessage = loginError.length ? <Alert severity="error">{loginError}</Alert> : ''
-
-  const loginTextField = <TextField label="Login" variant="standard" type="text" size="small" {...register('login')} />
-
-  const confirmPasswordTextField = (
-    <TextField
-      label="Confirm password"
+  const loginTextField = (
+    <Field as={TextField} name="login" id="login" label="Login" variant="standard" type="text" size="small" />
+  )
+  const emailTextField = (
+    <Field
+      as={TextField}
+      name="email"
+      id="email"
+      label="Email"
       variant="standard"
-      required={true}
+      required
+      placeholder="John.Doe@gmail.com"
+      type="text"
+      size="small"
+    />
+  )
+
+  const passwordTextField = (
+    <Field
+      as={TextField}
+      name="password"
+      id="password"
+      label="Password"
+      variant="standard"
+      required
       type="password"
       size="small"
-      error={'confirmPassword' in errors}
-      helperText={'confirmPassword' in errors ? 'The confirmation password does not match' : ''}
-      {...register('confirmPassword')}
     />
   )
 
-  const emailTextFieldReg = (
-    <TextField
-      label="Email"
+  const confirmPasswordTextField = (
+    <Field
+      as={TextField}
+      name="confirmPassword"
+      id="confirmPassword"
+      label="Confirm password"
       variant="standard"
-      required={true}
-      placeholder="John.Doe@gmail.com"
-      type="text"
+      required
+      type="password"
       size="small"
-      error={'email' in errors}
-      helperText={'email' in errors ? 'Email must be valid' : ''}
-      {...register('email')}
-    />
-  )
-
-  const emailTextFieldLog = (
-    <TextField
-      label="Email"
-      variant="standard"
-      required={true}
-      placeholder="John.Doe@gmail.com"
-      type="text"
-      size="small"
-      {...register('email')}
+      error={formik.touched.confirmPassword && Boolean(formik.errors.confirmPassword)}
+      helperText={
+        formik.touched.confirmPassword && formik.errors.confirmPassword && 'The confirmation password does not match'
+      }
     />
   )
 
@@ -182,25 +169,19 @@ export default function LoginForm({ formType }: { formType: string }) {
         <div className={styles.logo}>
           <img src={logo} className={styles['App-logo']} alt="logo" />
         </div>
-        <form onSubmit={handleSubmit(onSubmit)}>
-          {formType === 'registration' ? loginTextField : ''}
-          {formType === 'registration' ? emailTextFieldReg : emailTextFieldLog}
-          <TextField
-            label="Password"
-            variant="standard"
-            required={true}
-            type="password"
-            size="small"
-            {...register('password')}
-          />
-          {formType === 'registration' ? confirmPasswordTextField : ''}
-          {alertMessage}
-          {loginMessage}
-          <Button variant="contained" className={styles.button} type="submit">
-            {formType === 'registration' ? 'Sign up' : 'Login'}
-          </Button>
-          {LinkContainer}
-        </form>
+        <FormikProvider value={formik}>
+          <form onSubmit={formik.handleSubmit}>
+            {formType === 'registration' ? loginTextField : ''}
+            {emailTextField}
+            {passwordTextField}
+            {formType === 'registration' ? confirmPasswordTextField : ''}
+            {loginMessage}
+            <Button variant="contained" color="info" className={styles.button} type="submit">
+              {formType === 'registration' ? 'Sign up' : 'Login'}
+            </Button>
+            {LinkContainer}
+          </form>
+        </FormikProvider>
       </div>
     </div>
   )
